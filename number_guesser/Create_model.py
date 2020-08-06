cd90f8592e2d4d9da2f9ba062ae692c106f7a917
yashwant94308 <yashwant94308@gmail.com>
2020-08-06 21:48:23 +0530

    machine learning

---
+++ number_guesser/Create_model.py
@@ -0,0 +1,33 @@
+import install_requirements
+
+import keras
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+for train in range(len(x_train)):
+    for row in range(28):
+        for x in range(28):
+            if x_train[train][row][x] != 0:
+                x_train[train][row][x] = 1
+
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+model.save('epic_num_reader.model')
+
+print("Model saved")
